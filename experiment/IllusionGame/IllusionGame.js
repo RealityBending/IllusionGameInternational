5c613db09565f75812ac1834679fe286f66092b7
Dominique Makowski <dom.mak19@gmail.com>
2023-10-26 21:40:02 +0100

    add IG practice

--- experiment/IllusionGame/IllusionGame.js
+++ experiment/IllusionGame/IllusionGame.js
@@ -21,8 +21,8 @@ var mullerlyer_instructions =
     "<p>Your task is to select which <b>line is longer</b> in length as fast as you can, without making errors.</p>" +
     "<p>Don't get distracted by the surrounding black arrows at the end of the red lines!</p>" +
     "<p>Press <b>the UP or the DOWN arrow</b> to indicate where is the longer <b>red line.</b></p>" +
-    "<div style='float: center'><img src='materials/instructions/MullerLyer_Demo.png' height='200'></img>" +
-    "<p><img src='images/answer_updown_keyboard.PNG' height='100'></img></p>" +
+    "<div style='float: center'><img src='IllusionGame/images/MullerLyer_Demo.png' height='200'></img>" +
+    "<p><img src='IllusionGame/images/answer_updown_keyboard.PNG' height='100'></img></p>" +
     "<p class='small'>In this example, the correct answer is the <b>UP arrow</b>.</p></div>" +
     "<p>Are you ready? <b>Press ENTER to start</b></p>"
 
@@ -31,8 +31,8 @@ var ebbinghaus_instructions =
     "<p>Your task is to select which <b>red circle is bigger</b> in size as fast as you can, without making errors.</p>" +
     "<p>Don't get distracted by the surrounding black circles around the red circles!</p>" +
     "<p>Press <b>the LEFT or the RIGHT arrow</b> to indicate which is the bigger <b>red circle.</b></p>" +
-    "<div style='float: center'><img src='materials/instructions/Ebbinghaus_Demo.png' height='200'></img>" +
-    "<p><img src='images/answer_leftright_keyboard.PNG' height='100'></img></p>" +
+    "<div style='float: center'><img src='IllusionGame/images/Ebbinghaus_Demo.png' height='200'></img>" +
+    "<p><img src='IllusionGame/images/answer_leftright_keyboard.PNG' height='100'></img></p>" +
     "<p class='small'>In this example, the correct answer is the <b>LEFT arrow</b>.</p></div>" +
     "<p>Are you ready? <b>Press ENTER to start</b></p>"
 
@@ -41,8 +41,8 @@ var verticalhorizontal_instructions =
     "<p>Your task is to tell <b>which line is longer</b> in length, regardless of their orientation, as fast as you can, and without making errors.</p>" +
     "<p>Don't get distracted by the orientation of the lines!</p>" +
     "<p>Press <b>the LEFT or the RIGHT arrow</b> to indicate which <b>line is the longer one.</b></p>" +
-    "<div style='float: center'><img src='materials/instructions/VerticalHorizontal_Demo.png' height='200'></img>" +
-    "<p><img src='images/answer_leftright_keyboard.PNG' height='100'></img></p>" +
+    "<div style='float: center'><img src='IllusionGame/images/VerticalHorizontal_Demo.png' height='200'></img>" +
+    "<p><img src='IllusionGame/images/answer_leftright_keyboard.PNG' height='100'></img></p>" +
     "<p class='small'>In this example, the correct answer is the <b>LEFT arrow</b>.</p></div>" +
     "<p>Are you ready? <b>Press ENTER to start</b></p>"
 
@@ -331,7 +331,7 @@ function IG_make_trials(
 
     // Set stimuli (var stimuli is loaded in stimuli/stimuli.js)
     var stim_list = stimuli.filter(
-        (stimuli) => stimuli.Illusion_Type === illusion_name
+        (stimuli) => stimuli.data.Illusion_Type === illusion_name
     )
 
     // Preload images
